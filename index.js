e7d306f5c4d4832802219bf00f476a94fee55f57
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-02-19 15:29:49 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+const samson = new Scooter ("2015", "red", "swoosh")
+
+function Driver(name, age, experience) {
+    this.name = "Allison";
+    this.age = "16 years";
+    this.experience = "2 years";
+}
+
+
+function PickupLocation(address, city) {
+    this.address = "123 Broadway";
+    this.city = "New York City";
+    
+}
